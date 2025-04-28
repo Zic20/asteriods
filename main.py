87ab2ed2b53110a437b2d5f0153868f7cb5fe3ab
zico <izallyjr@gmail.com>
2025-04-28 00:07:00 -0400

    added player shooting ability

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 
 def main():
@@ -11,12 +12,18 @@ def main():
    clock = pygame.time.Clock()
    dt = 0
    running = True
+   
+   ##Groups##
    updatable = pygame.sprite.Group()
    drawable = pygame.sprite.Group()
    asteroids = pygame.sprite.Group()
+   shots = pygame.sprite.Group()
+   ##Groups end##
+   
    Asteroid.containers = (asteroids,updatable,drawable)
    AsteroidField.containers = (updatable)
    Player.containers = (updatable,drawable)
+   Shot.containers = (shots,updatable,drawable)
    player = Player(SCREEN_WIDTH /2,SCREEN_HEIGHT/2)
    asteroidField = AsteroidField()
    while running:
